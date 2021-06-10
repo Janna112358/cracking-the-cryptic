3c01e0217109dc9836151de1dcf5eb2c4fa7f778
Janna Goldstein <jgoldstein@star.bham.ac.uk>
2021-06-10 09:13:42 +0200

    add checker for no repeated digits in a box or domino, and find all dominoes without repeats

--- README.md
+++ README.md
@@ -28,3 +28,6 @@ digits in each row.
 Because there is a "triple domino" i.e three boxes next to each other overlapping
 in the puzzle, the script computes all possible combinations of boxes that do so.
 These results are saved in the file triple_dominoes.txt (also [in this repository](https://github.com/Janna112358/cracking-the-cryptic/blob/master/triple_dominoes.txt)).
+There is also a domino which sits entirely within one box of the sudoku, and therefore
+can have none of its digits repeated. Possibilities for this are checked and saved in
+no_repeat_dominoes.txt (also [in this repository](https://github.com/Janna112358/cracking-the-cryptic/blob/master/no_repeat_dominoes.txt))
--- deep_thought.py
+++ deep_thought.py
@@ -24,6 +24,14 @@ class Box:
     def __repr__(self):
         return f"\n{self.topLeft} | {self.topRight}\n{self.bottomLeft} | {self.bottomRight}\n"
         
+    def hasNoRepeats(self):
+        """
+        Check if this box could fit within a single box of the sudoku, i.e. has
+        no repeated digits
+        """
+        return (self.topLeft != self.bottomRight and 
+                self.bottomLeft != self.topRight)
+    
     def doesDominoRight(self, other):
         """
         Check if this box can "domino" (overlap) with another box on its right
@@ -39,6 +47,24 @@ class Box:
         Check if this box can "domino" (overlap) with another box on its left
         """
         return other.doesDominoRight(self)
+    
+    def doesDominoRightNoRepeat(self, other):
+        """
+        Check if this box can "domino" with another no its right, without having
+        other digits repeated.
+        """
+        noRepeats = (self.hasNoRepeats() and 
+                     other.hasNoRepeats() and 
+                     self.topLeft != other.bottomRight and 
+                     self.bottomLeft != other.topRight)
+        return noRepeats and self.doesDominoRight(other)
+    
+    def doesDominoLeftNoRepeat(self, other):
+        """
+        Check if this box can "domino" with another no its left, without having
+        other digits repeated.
+        """
+        return other.doesDominoRightNoRepeat(self)
 
     @classmethod
     def createIfValid(self, topLeft, topRight): 
@@ -76,6 +102,23 @@ class Box:
                     allValidBoxes.append(box)
         return allValidBoxes
     
+def findNoRepeatDominoes():
+    """Find all combinations of boxes that can "domino" and fit within
+    a sudoku box.
+    
+    They have to overlap on one column to domino. To fit within a sudoku box,
+    there can be no repeat digits (which is relevant for the other columns). 
+    """ 
+    dominoes = []
+    
+    boxes = Box.createAllValid()
+    for box1, box2 in itertools.product(boxes, boxes):
+        if box1 is box2:
+            continue
+        if box1.doesDominoRightNoRepeat(box2):
+            dominoes.append([box1, box2])
+    return dominoes
+    
     
 def findTripleDominoes():
     triples = []
@@ -109,13 +152,22 @@ if __name__ == "__main__":
         for box in boxes:
             outputFile.write(str(box))
             
+    noRepeatDominoes = findNoRepeatDominoes()
+    print(f"Found {len(noRepeatDominoes)} dominoes without repeats.")
+    
+    with open('no_repeat_dominoes.txt', 'w+') as outputFile2:
+        for idx, domino in enumerate(noRepeatDominoes):
+            outputFile2.write(f"\nNo repeat domino #{idx+1}")
+            for box in domino:
+                outputFile2.write(str(box))
+            
     tripleDominoes = findTripleDominoes()
     print(f"Found {len(tripleDominoes)} triple dominoes!")
     
-    with open('triple_dominoes.txt', 'w+') as outputFile: 
+    with open('triple_dominoes.txt', 'w+') as outputFile3: 
         for idx, domino in enumerate(tripleDominoes):
-            outputFile.write(f"\nTriple domino #{idx+1}")
+            outputFile3.write(f"\nTriple domino #{idx+1}")
             for box in domino:
-                outputFile.write(str(box))
+                outputFile3.write(str(box))
             
             
